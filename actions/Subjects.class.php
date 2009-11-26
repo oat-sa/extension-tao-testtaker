a3eb1806db1376becb7950dcad9ccd3ab4719557
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-11-26 16:37:05 +0000

    implement lists and constants
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@1674 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/Subjects.class.php
+++ actions/Subjects.class.php
@@ -212,6 +212,18 @@ class Subjects extends TaoModule {
 		}
 	}
 	
+	public function getLists(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		
+		echo json_encode(
+			$this->getListData(array(
+				TAO_SUBJECT_CLASS
+			))
+		);
+	}
+	
 	/*
 	 * @TODO implement the following actions
 	 */
--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,14 @@
 <?php
-define('TAO_SUBJECT_CLASS',				'http://www.tao.lu/Ontologies/TAOSubject.rdf#Subject');
-define('TAO_SUBJECT_NAMESPACE',			'http://www.tao.lu/Ontologies/TAOSubject.rdf');
-define('LOCAL_NAMESPACE',				'http://127.0.0.1/middleware/demoSubjects.rdf');
-define('TAO_OBJECT_CLASS',				'http://www.tao.lu/Ontologies/TAO.rdf#TAOObject');
+$todefine = array(
+	'TAO_SUBJECT_CLASS' 	=> 'http://www.tao.lu/Ontologies/TAOSubject.rdf#Subject',
+	'TAO_SUBJECT_NAMESPACE' => 'http://www.tao.lu/Ontologies/TAOSubject.rdf',
+	'LOCAL_NAMESPACE' 		=> 'http://127.0.0.1/middleware/demoSubjects.rdf',
+	'TAO_OBJECT_CLASS'		=> 'http://www.tao.lu/Ontologies/TAO.rdf#TAOObject'
+);
+foreach($todefine as $constName => $constValue){
+	if(!defined($constName)){
+		define($constName, $constValue);
+	}
+}
+unset($todefine);
 ?>
