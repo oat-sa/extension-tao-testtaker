2da5452d1a7c32fe40c4888ca9e7890896637972
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-10 17:38:14 +0000

    clean config.php, most of them are now empty
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@1776 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -1,34 +1,10 @@
 <?php
 
-session_start();
+
 
 require_once 	dirname(__FILE__). "/config.php";
 require_once 	dirname(__FILE__). "/constants.php";
 
-
-
-/**
- * @function tao_autoload
- * permits to include classes automatically using the ARGOUML class naming convention
- * @param 	string		$pClassName		Name of the class
- */
-function tao_autoload($pClassName) {
-
-	$split = split("_",$pClassName);
-	$path = GENERIS_BASE_PATH.'/';
-	for ( $i = 0 ; $i<sizeof($split)-1 ; $i++){
-		$path .= $split[$i].'/';
-	}
-
-	$filePath = $path . 'class.'.$split[sizeof($split)-1] . '.php';
-	if (file_exists($filePath)){
-		require_once $filePath;
-	}
-
-}
-
-
-spl_autoload_register("tao_autoload");
 
 
 set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH);
--- manifest.php
+++ manifest.php
@@ -9,7 +9,14 @@
 			'install' => array( 
 				'sql' => dirname(__FILE__). '/model/ontology/taosubjects.sql',
 				'php' => dirname(__FILE__). '/install/install.php'
-			)
+			),
+			'configFile' => dirname(__FILE__). '/includes/common.php',
+
+			'classLoaderPackages' => array( 
+				dirname(__FILE__).'/actions/',
+				dirname(__FILE__).'/helpers/'
+			 )
+
 			
 
 				
