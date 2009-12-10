3b37e5bb44491a4d781dc8ea1ed2f6e85e38f85d
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-10 17:21:28 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@1775 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -9,14 +9,7 @@
 			'install' => array( 
 				'sql' => dirname(__FILE__). '/model/ontology/taosubjects.sql',
 				'php' => dirname(__FILE__). '/install/install.php'
-			),
-			'configFile' => dirname(__FILE__). '/includes/common.php',
-
-			'classLoaderPackages' => array( 
-				dirname(__FILE__).'/actions/',
-				dirname(__FILE__).'/helpers/'
-			 )
-
+			)
 			
 
 				
