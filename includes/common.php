b35606a4deaf1bcf7b213a54d2e2741f0b752e53
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-11 15:44:37 +0000

    Many stuff  remove config from manifest and resolve autoload problems
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@1781 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -7,7 +7,7 @@ require_once 	dirname(__FILE__). "/constants.php";
 
 
 
-set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH);
+set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH.'/..');
 
 core_control_FrontController::connect(API_LOGIN, API_PASS, API_MODULE);
 core_kernel_classes_Session::singleton()->setLg($GLOBALS['lang']);
