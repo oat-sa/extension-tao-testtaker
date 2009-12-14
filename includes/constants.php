665a8e3468990fed2e779fb6323cf680fd0a21c4
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-14 13:34:06 +0000

    update rdf namespaces
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@1791 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 $todefine = array(
 	'TAO_SUBJECT_CLASS' 	=> 'http://www.tao.lu/Ontologies/TAOSubject.rdf#Subject',
 	'TAO_SUBJECT_NAMESPACE' => 'http://www.tao.lu/Ontologies/TAOSubject.rdf',
-	'LOCAL_NAMESPACE' 		=> 'http://127.0.0.1/middleware/demoSubjects.rdf',
+	'LOCAL_NAMESPACE' 		=> 'http://127.0.0.1/middleware/demo.rdf',
 	'TAO_OBJECT_CLASS'		=> 'http://www.tao.lu/Ontologies/TAO.rdf#TAOObject',
 	'GENERIS_BOOLEAN'		=> 'http://www.tao.lu/Ontologies/generis.rdf#Boolean'
 );
