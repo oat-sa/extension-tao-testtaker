ce4760611df6c0e19a73531112e2764205ef8fb3
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-23 10:02:37 +0000

    fix get instance of subclass bug
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@1934 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/class.SubjectsService.php
+++ models/classes/class.SubjectsService.php
@@ -165,6 +165,14 @@ class taoSubjects_models_classes_SubjectsService
 
         // section 10-13-1-45-792423e0:12398d13f24:-8000:0000000000001818 begin
 		
+		if(is_null($clazz) && $mode == 'uri'){
+			try{
+				$resource = new core_kernel_classes_Resource($identifier);
+				$type = $resource->getUniquePropertyValue(new core_kernel_classes_Property( RDF_TYPE ));
+				$clazz = new core_kernel_classes_Class($type->uriResource);
+			}
+			catch(Exception $e){}
+		}
 		if(is_null($clazz)){
 			$clazz = $this->subjectClass;
 		}
