098bd1959ebafd236d88e21d07abea1564ca89e3
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-06-08 12:45:45 +0000

    * refactor Trees component to load the data asynchronously (by opening the sub nodes)
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@2761 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/Subjects.class.php
+++ actions/Subjects.class.php
@@ -197,8 +197,15 @@ class Subjects extends TaoModule {
 		if(!tao_helpers_Request::isAjax()){
 			throw new Exception("wrong request mode");
 		}
-		
-		echo json_encode($this->service->toTree( new core_kernel_classes_Class(TAO_GROUP_CLASS), true, true, ''));
+		$options = array('chunk' => false);
+		if($this->hasRequestParameter('classUri')) {
+			$clazz = $this->getCurrentClass();
+			$options['chunk'] = true;
+		}
+		else{
+			$clazz = new core_kernel_classes_Class(TAO_GROUP_CLASS);
+		}
+		echo json_encode($this->service->toTree($clazz, $options));
 	}
 	
 	/**
