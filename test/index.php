e205bf34d97968061b56f857401334ee72cd4224
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-08-11 13:42:41 +0000

    * update unti testing mode: one suite by module
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@3061 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ test/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once dirname(__FILE__) . '/../../tao/test/TestRunner.php';
+
+//get the test into each extensions
+$tests = TestRunner::getTests(array('taoSubjects'));
+
+//create the test sutie
+$testSuite = new TestSuite('TAO Subject unit tests');
+foreach($tests as $testCase){
+	$testSuite->addTestFile($testCase);
+}    
+
+//add the reporter regarding the context
+if(PHP_SAPI == 'cli'){
+	$reporter = new XmlTimeReporter();
+}
+else{
+	$reporter = new HtmlReporter();
+}
+//run the unit test suite
+$testSuite->run($reporter);
+?>
