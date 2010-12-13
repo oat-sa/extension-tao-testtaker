c13d7cef6e6b15051a3f99984452be1bd019d726
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-12-13 18:22:21 +0000

    All extensions:  * update the bootstrap to begin the session in paramter if set (used by the file uploader and the curl requests)  TAO Items QTI:  * implement the grapic order interaction  * begin to implement the upload interaction
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@3951 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -4,6 +4,10 @@
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * 
  */
+$request = new Request();
+if($request->hasParameter('session_id')){
+ 	session_id($request->getParameter('session_id'));
+}
 session_start();
 
 require_once dirname(__FILE__). '/config.php';
@@ -11,5 +15,6 @@ require_once dirname(__FILE__). '/constants.php';
 
 set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH.'/..');
 
+require_once 'tao/helpers/class.Uri.php';
 include_once 'tao/includes/prepend.php';
 ?>
