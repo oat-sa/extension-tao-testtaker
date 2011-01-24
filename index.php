6b923f1730d71381aa916c53f2b4480637f645cf
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-24 17:20:44 +0000

    !!! MODEL AND DB REFACTORING + CLEANING UP !!!  !!! BOOTSTRAP FULL REFACTORING !!!
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@4282 62dfeb86-a93f-4914-9d13-15c0a160985d

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  */
-include dirname(__FILE__). '/../tao/includes/class.Bootstrap.php';
+require_once dirname(__FILE__). '/../tao/includes/class.Bootstrap.php';
 
 $bootStrap = new BootStrap('taoSubjects');
 $bootStrap->start();
