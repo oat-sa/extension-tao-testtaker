09881f58de018bbf19899840a98614813457719c
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-03-29 17:19:53 +0000

    Update the workflow services
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@5176 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ actions/class.SasSubjectsImport.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Extends the common Import class to update the behavior
+ * 
+ * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @package taoSubjects
+ * @subpackage actions
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * 
+ */
+
+class taoSubjects_actions_SasSubjectsImport extends taoSubjects_actions_SubjectsImport {
+
+ 	/**
+     * @see Subjects::__construct()
+     */
+    public function __construct() {
+    	tao_helpers_Context::load('STANDALONE_MODE');
+        $this->setSessionAttribute('currentExtension', 'taoSubjects');
+		parent::__construct();
+    }
+	
+	/**
+	 * @see TaoModule::setView()
+	 * @param string $identifier the view name
+	 * @param boolean $useMetaExtensionView use a view from the parent extention
+	 * @return mixed 
+	 */
+    public function setView($identifier, $useMetaExtensionView = false) {
+		if(tao_helpers_Request::isAjax()){
+			return parent::setView($identifier, $useMetaExtensionView);
+		}
+    	if($useMetaExtensionView){
+			$this->setData('includedView', $identifier);
+		}
+		else{
+			$this->setData('includedView', BASE_PATH . '/' . DIR_VIEWS . $GLOBALS['dir_theme'] . $identifier);
+		}
+		return parent::setView('sas.tpl', true);
+    }
+    
+}	
+?>
