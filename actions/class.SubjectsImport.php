851f4a5b89537ed0ab9ca7b2aaec40212ed7e6e2
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-07-28 15:36:24 +0000

    - "callbacks" option added to the class "tao_helpers_data_GenerisAdapterCsv": to map properties to the preprocessing callback functions - added 2 new properties to tao_actions_Import: $excludedProperties and $additionalAdapterOptions - taoSubjects_actions_SubjectsImport modified to exclude the PROPERTY_USER_DEFLG, in order to set it as the system's value
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@6536 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.SubjectsImport.php
+++ actions/class.SubjectsImport.php
@@ -12,14 +12,36 @@
 
 class taoSubjects_actions_SubjectsImport extends tao_actions_Import {
 
-	/**
-	 * Add static data to each imported subjects,
-	 * here we add the subject role as 2nd Type 
-	 * @var array
-	 */
-	protected $staticData = array(
-		RDF_TYPE	=> CLASS_ROLE_SUBJECT
-	);
+	
+	protected $excludedProperties = array(PROPERTY_USER_DEFLG);
+	protected $additionalAdapterOptions = array();
+	
+	public function __construct(){
+		
+		parent::__construct();
+		
+		//Add static data to each imported subjects, here we add the subject role as 2nd Type plus the mendatory system default language
+		$lang = '';
+		$langResource = tao_helpers_I18n::getLangResourceByCode(DEFAULT_LANG);
+		if($langResource instanceof core_kernel_classes_Resource){
+			$lang = $langResource->uriResource;
+		}else{
+			throw new Exception('cannot find the default system language during subjects import');
+		}
+		
+		$this->staticData = array(
+			RDF_TYPE	=> CLASS_ROLE_SUBJECT,
+			PROPERTY_USER_DEFLG => $lang
+		);
+		
+		$this->additionalAdapterOptions = array(
+			'callbacks' => array(
+				'*' => array('trim'),
+				PROPERTY_USER_PASSWORD => array('md5')
+			)
+		);
+		
+	}
 	
 }
 ?>
