be6187922e3a2698cabfe64210407367cf011588
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-05-23 12:18:28 +0000

    changed structures.xml to allow several structures per extension removed meta-extension User
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@11208 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ actions/class.Main.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * 
+ * 
+ * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * @package taoItems
+ * @subpackage actions
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ */
+class taoSubjects_actions_Main extends tao_actions_Main {
+}
+?>
