4b8fa3ad74a70d5fed47abad889e5d4f11b3623b
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-06-15 12:19:16 +0000

    updated extension nrs to 2.3 removed force smooth for versions pre 2.1
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@11488 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -8,7 +8,7 @@ return array(
 	'name' => 'taoSubjects',
 	'description' => 'TAO Subjects extension',
 	'additional' => array(
-		'version' => '2.0',
+		'version' => '2.3',
 		'author' => 'CRP Henri Tudor',
 		'dependances' => array('tao'),
 		'extends' => 'tao',
