fbcb45e5caf6fe67bdae5f575dec7b8913f0118a
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-09-19 14:08:07 +0000

    * redirected getTree/getActions to extension for translation * fixed manifests
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@12162 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ actions/class.Main.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @package taoSubjects
+ * @subpackage actions
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * 
+ */
+
+class taoSubjects_actions_Main extends tao_actions_Main {
+	
+}
+?>
--- manifest.php
+++ manifest.php
@@ -45,7 +45,7 @@ return array(
 		'DEFAULT_ACTION_NAME'	=> 'index',
 	
 		#BASE PATH: the root path in the file system (usually the document root)
-		'BASE_PATH'				=> $extpath . 'taoSubjects',
+		'BASE_PATH'				=> $extpath,
 	
 		#BASE URL (usually the domain root)
 		'BASE_URL'				=> ROOT_URL . '/taoSubjects',
