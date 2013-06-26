9b4898816c8276366428338ffe18f4a1f9aee268
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-06-26 15:58:11 +0000

    unit test for restsubjects get/put tested
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@14949 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/RestSubjectsTestCase.php
+++ test/RestSubjectsTestCase.php
@@ -38,18 +38,16 @@ class RestSubjectsTestCase extends UnitTestCase {
 	 * tests initialization
 	 */
 	public function setUp(){		
-		TaoTestRunner::initTest();
+		    TaoTestRunner::initTest();
 		    //creates a user using remote script from joel
 		    $process = curl_init($this->host.'/tao/test/connector/setUp.php');
 		    curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
 		    $returnedData = curl_exec($process);
 		    $data = json_decode($returnedData, true);
 		    $this->assertNotNull($data);
-		  
 		    $this->login = $data['userData'][PROPERTY_USER_LOGIN];
 		    $this->password = $data['userData'][PROPERTY_USER_PASSWORD];
 		    $this->userUri			= $data['userUri'];
-		    
 	}
 	public function tearDown(){
 	    //removes the created user
@@ -58,7 +56,44 @@ class RestSubjectsTestCase extends UnitTestCase {
 		    curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
 		     $data = curl_exec($process);
 	}
-	public function testAuth(){
+	/**
+	 * shall be used beyond high level http connections unit tests (default parameters)
+	 * @param returnType CURLINFO_HTTP_CODE, etc... (default returns rhe http response data
+	 *
+	 */
+	private function curl($url, $method = CURLOPT_HTTPGET, $returnType = "data", $curlopt_httpheaders = array()){
+	     $process = curl_init($url);
+	     curl_setopt($process, $method, 1);
+	     curl_setopt($process, CURLOPT_USERPWD, $this->login.":".$this->password);
+	     curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+	     
+	     $headers = array_merge(array (
+		 "Accept: application/json"
+		 ), $curlopt_httpheaders);
+	     curl_setopt($process,CURLOPT_HTTPHEADER, $headers);
+	    if ($method==CURLOPT_POST) {
+		curl_setopt($process, CURLOPT_POSTFIELDS, "");
+		
+	    }
+	     //curl_setopt($process,CURLOPT_HTTPHEADER,$curlopt_httpheaders);
+	     $data = curl_exec($process);
+	     if ($returnType != "data"){
+		 $data = curl_getinfo($process, $returnType);
+	     }
+	     curl_close($process);
+	     return $data;
+	}
+
+	private function checkPropertyValues($propertyValues, $property, $valueType="literal", $value){
+	     foreach ($propertyValues as $propertyValue) {
+		if ($propertyValue["predicateUri"] == $property){
+		    
+		    $this->assertEqual($propertyValue["values"][0]["valueType"], $valueType);
+		    $this->assertEqual($propertyValue["values"][0]["value"],  $value);
+		}
+	    }
+	}
+	public function testHttp(){
 	    
 	    $url = $this->host.'taoSubjects/RestSubjects';
 	    //HTTP Basic
@@ -87,17 +122,7 @@ class RestSubjectsTestCase extends UnitTestCase {
 	    $this->assertEqual($http_status, "401");
 	    curl_close($process);
 
-	    //should return a 200
-	    $process = curl_init($url);
-	     curl_setopt($process,CURLOPT_HTTPHEADER,array (
-		 "Accept: application/json"
-		 ));
-	     
-	     curl_setopt($process, CURLOPT_USERPWD, $this->login.":".$this->password);
-	     curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-	    $data = curl_exec($process);
-
-	      //should return a 406
+	    //should return a 406
 	     $process = curl_init($url);
 	     curl_setopt($process,CURLOPT_HTTPHEADER,array (
 		 "Accept: dummy/dummy"
@@ -107,8 +132,9 @@ class RestSubjectsTestCase extends UnitTestCase {
 	    $data = curl_exec($process);
 	    $http_status = curl_getinfo($process, CURLINFO_HTTP_CODE);
 	    $this->assertEqual($http_status, "406");
+	    curl_close($process);
 
-	     //should return a 200
+	         //should return a 200
 	    $process = curl_init($url);
 	     curl_setopt($process,CURLOPT_HTTPHEADER,array (
 		 "Accept: application/xml"
@@ -118,9 +144,10 @@ class RestSubjectsTestCase extends UnitTestCase {
 	    $data = curl_exec($process);
 	    $http_status = curl_getinfo($process, CURLINFO_HTTP_CODE);
 	    $this->assertEqual($http_status, "200");
-	     curl_close($process);
-	    
-	    //should return a 200, should return content encoding application/xml
+
+
+
+	      //should return a 200, should return content encoding application/xml
 	     $process = curl_init($url);
 	     curl_setopt($process,CURLOPT_HTTPHEADER,array (
 		 "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
@@ -133,6 +160,73 @@ class RestSubjectsTestCase extends UnitTestCase {
 	     $contentType = curl_getinfo($process, CURLINFO_CONTENT_TYPE);
 	     $this->assertEqual( $contentType, "application/xml");
 	    curl_close($process);
+
+	    //should return a 200
+	    $http_status = $this->curl($url, CURLOPT_HTTPGET, CURLINFO_HTTP_CODE);
+	    $this->assertEqual($http_status, "200");
+
+	}
+
+	public function testCrud(){
+
+	    //get the complete list (should be empty)
+	    $url = $this->host.'taoSubjects/RestSubjects';
+	    $returnedData = $this->curl($url);
+	    $data = json_decode($returnedData, true);
+	    $this->assertEqual( $data["success"], true);
+
+	    //create a new test taker without aprameters, should return a 400
+	    $http_status = $this->curl($url, CURLOPT_POST, CURLINFO_HTTP_CODE);
+	    $this->assertEqual($http_status, "400");
+
+	    //login but no password, should return a 400
+	    $http_status = $this->curl($url, CURLOPT_POST, CURLINFO_HTTP_CODE, array('login: dummy'));
+	    $this->assertEqual($http_status, "400");
+
+	    //should be 200
+	    $genLogin = 'dummy'.rand(0,65535);
+	   $returnedData = $this->curl($url, CURLOPT_POST, "data", array('login: '.$genLogin, 'password: dummy'));
+	   $data = json_decode($returnedData, true);
+	    $this->assertEqual( $data["success"], true);
+	   $uriSubject = $data["data"]["uriResource"];
+	    //get this test taker
+	     $returnedData = $this->curl($url, CURLOPT_HTTPGET, "data", array('uri: '.$uriSubject));
+	    $data = json_decode($returnedData, true);
+	    $this->assertEqual( $data["success"], true);
+	    $this->assertEqual( $data["data"]["uri"], $uriSubject);
+
+	    $this->checkPropertyValues($data["data"]["properties"], PROPERTY_USER_LOGIN, "literal", $genLogin);
+	    $this->checkPropertyValues($data["data"]["properties"], PROPERTY_USER_PASSWORD, "literal", md5('dummy'));
+
+	    //modifying the login of a subject is not allowed : 412
+	     $returnedData = $this->curl($url, CURLOPT_PUT, CURLINFO_HTTP_CODE, array('uri: '.$uriSubject, 'login: blabla'));
+	     $this->assertEqual( $returnedData, 412);
+	    //get all test takers
+	     //modifying the login of a subject is not allowed : 412
+	     $returnedData = $this->curl($url, CURLOPT_PUT, CURLINFO_HTTP_CODE, array('uri: '.$uriSubject, 'password: blabla'));
+	     $this->assertEqual( $returnedData, 200);
+	    //edit this test taker
+	     $returnedData = $this->curl($url, CURLOPT_PUT, "data", array('uri: '.$uriSubject, 'firstName: patrick','password: blabla'));
+	     $returnedData = $this->curl($url, CURLOPT_HTTPGET, "data", array('uri: '.$uriSubject));
+	    $data = json_decode($returnedData, true);
+	   
+	    $this->assertEqual( $data["success"], true);
+	    $this->assertEqual( $data["data"]["uri"], $uriSubject);
+	    $this->checkPropertyValues($data["data"]["properties"], PROPERTY_USER_LOGIN, "literal", $genLogin);
+	    $this->checkPropertyValues($data["data"]["properties"], PROPERTY_USER_PASSWORD, "literal", md5('blabla'));
+	    $this->checkPropertyValues($data["data"]["properties"], PROPERTY_USER_LASTNAME, "literal", 'patrick');
+	    
+	      //get this test taker
+	     
+
+	    //remove this test taker
+
+	    //get all test takers
+
+	    //
+
+
+	    
 	}
 
 }
