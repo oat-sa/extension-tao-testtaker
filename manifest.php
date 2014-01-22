4f83356caeda8fe06476eaa615e743f21687495c
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-01-22 14:53:15 +0000

    added license support to manifest
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@18036 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -32,7 +32,8 @@ $taopath = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'tao'.DIRECTORY_SEPARA
 return array(
 	'name' => 'taoSubjects',
 	'description' => 'TAO Subjects extension',
-	'version' => '2.4',
+    'license' => 'GPL-2.0',
+    'version' => '2.4',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'dependencies' => array('tao'),
 	'models' => array(
