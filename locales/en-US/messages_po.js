791850bdd97833f738baaf6ec78ebc5ba1baab9e
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-05 15:10:40 +0000

    changed description (ran taoTranslate.php -v -a compile -l en-US -e taoSubjects and update )
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoSubjects/trunk@20060 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : {"Edit subject":"Edit test taker","Edit subject class":"Edit test taker class"}
+   i18n_tr : {"Edit subject":"Edit test taker","Edit subject class":"Edit test taker class","Test takers may be assigned to sets according to the relevant assessment context (Human resources management, education, edumetric monitoring, etc.).":"Assign test-takers to groups before scheduling tests for delivery.\u0000","The Test takers module allows to record and manage  test takers.":"Create and manage test-takers with as many properties as needed, such as name, address, gender, age, etc.\u0000"}
  };
 });
