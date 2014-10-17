a10dfbccef8c1b43aa54ebea4e49051769b1581b
Bertrand Chevrier <bertrand@taotesting.com>
2014-10-17 16:42:16 +0200

    showNodeUri from session to selectNode variable

--- actions/TestTaker.php
+++ actions/TestTaker.php
@@ -175,11 +175,11 @@ class TestTaker extends \tao_actions_SaSModule
                     $this->redirect(_url('editSubject', null, null, $params));
                 }
                 
+                $this->setData("selectNode", \tao_helpers_Uri::encode($subject->getUri()));
                 $this->setData('message', $message);
                 $this->setData('reload', true);
             }
         }
-        $this->setSessionAttribute("showNodeUri", \tao_helpers_Uri::encode($subject->getUri()));
         
         if (\common_ext_ExtensionsManager::singleton()->isEnabled('taoGroups')) {
             $this->setData('groupForm', GroupForm::renderGroupTreeForm($subject));
@@ -207,7 +207,7 @@ class TestTaker extends \tao_actions_SaSModule
         if ($myForm->isSubmited()) {
             if ($myForm->isValid()) {
                 if ($clazz instanceof core_kernel_classes_Resource) {
-                    $this->setSessionAttribute("showNodeUri", \tao_helpers_Uri::encode($clazz->getUri()));
+                    $this->setData("selectNode", \tao_helpers_Uri::encode($clazz->getUri()));
                 }
                 $this->setData('message', __('Class saved'));
                 $this->setData('reload', true);
