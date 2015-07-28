5bcd7e734b65ecc08b3942e94957ce644f03dcd6
Lionel Lecaque <lionel@taotesting.com>
2015-07-28 15:42:37 +0200

    update version to 2.7

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'label' => 'Test-taker core extension',
 	'description' => 'TAO TestTaker extension',
     'license' => 'GPL-2.0',
-    'version' => '2.6.1',
+    'version' => '2.7',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
 	    'taoBackOffice' => '>=0.8'
