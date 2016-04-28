7916a4ccd04a0c09ba3a9af526c72811bc3e9ae6
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-04-28 12:14:48 +0200

    remove mediaelement

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
             modules : [{
                 name: 'taoTestTaker/controller/routes',
                 include : ext.getExtensionsControllers(['taoTestTaker']),
-                exclude : ['mathJax', 'mediaElement'].concat(libs)
+                exclude : ['mathJax'].concat(libs)
             }]
         }
     };
