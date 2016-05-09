e84a6e5ed411aef8c3d107905af7bf0bd573da4c
jsconan <jsconan@gmail.com>
2016-05-09 14:25:07 +0200

    Compile JS, update version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -39,7 +39,7 @@ class Updater extends \common_ext_ExtensionUpdater
 
 		$this->setVersion($currentVersion);
 		
-		$this->skip('2.7.1','2.8.0');
+		$this->skip('2.7.1','2.9.0');
 		return null;
 	}
 }
