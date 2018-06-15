b9e9879c093afb73ea06b8dcd15130e90244699b
Gyula Szucs <gyula@taotesting.com>
2018-06-15 13:26:36 +0200

    TAO-6401 Changed import/export events

--- actions/Import.php
+++ actions/Import.php
@@ -1,24 +1,25 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
  *               2002-2008 (update and modification) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- * 
+ *
  */
+
 namespace oat\taoTestTaker\actions;
 
 use common_report_Report;
@@ -33,52 +34,35 @@ use tao_helpers_form_FormFactory;
 /**
  * Extends the common Import class to exchange the generic
  * CsvImporter with a subject specific one
- * 
- * @author Bertrand Chevrier, <taosupport@tudor.lu>
- * @package c
- * 
+ *
+ * @author  Bertrand Chevrier, <taosupport@tudor.lu>
  */
-class Import extends \tao_actions_Import 
+class Import extends \tao_actions_Import
 {
-    
     /**
      * (non-PHPdoc)
      * @see tao_actions_Import::getAvailableImportHandlers()
      */
-	public function getAvailableImportHandlers() 
-	{
-		$returnValue = parent::getAvailableImportHandlers();
-		
-		foreach (array_keys($returnValue) as $key) {
-		    if ($returnValue[$key] instanceof \tao_models_classes_import_CsvImporter) {
-		        $importer =  new CsvImporter();
-				$importer->setValidators($this->getValidators());
-				$returnValue[$key] = $importer;
-		    }
-		}
-        		
-		return $returnValue;
-	}
-
-	protected function getValidators(){
-		return array(
-			GenerisRdf::PROPERTY_USER_LOGIN => array(tao_helpers_form_FormFactory::getValidator('Unique')),
-		);
-	}
-
-    /**
-     * @param common_report_Report $report
-     * @throws \core_kernel_persistence_Exception
-     */
-    protected function onAfterImport(common_report_Report $report)
+    public function getAvailableImportHandlers()
     {
-        /** @var common_report_Report $success */
-        foreach ($report->getSuccesses() as $success) {
-            $resource = $success->getData();
-            if ($resource instanceof core_kernel_classes_Resource) {
-                $this->getEventManager()->trigger(new TestTakerImportedEvent($resource->getUri(), $this->getProperties($resource)));
+        $returnValue = parent::getAvailableImportHandlers();
+
+        foreach (array_keys($returnValue) as $key) {
+            if ($returnValue[$key] instanceof \tao_models_classes_import_CsvImporter) {
+                $importer = new CsvImporter();
+                $importer->setValidators($this->getValidators());
+                $returnValue[$key] = $importer;
             }
         }
+
+        return $returnValue;
+    }
+
+    protected function getValidators()
+    {
+        return [
+            GenerisRdf::PROPERTY_USER_LOGIN => [tao_helpers_form_FormFactory::getValidator('Unique')],
+        ];
     }
 
     /**
@@ -94,9 +78,9 @@ class Import extends \tao_actions_Import
         $taoTestTaker = $extManager->getExtensionById('taoTestTaker');
         $config = $taoTestTaker->getConfig('csvImporterCallbacks');
 
-        if ((bool)$config['use_properties_for_event']){
+        if ((bool)$config['use_properties_for_event']) {
             return [
-                'hashForKey' => UserHashForEncryption::hash(TestTakerSavePasswordInMemory::getPassword()),
+                'hashForKey'                       => UserHashForEncryption::hash(TestTakerSavePasswordInMemory::getPassword()),
                 GenerisRdf::PROPERTY_USER_PASSWORD => $resource->getOnePropertyValue(
                     new \core_kernel_classes_Property(GenerisRdf::PROPERTY_USER_PASSWORD)
                 )->literal
