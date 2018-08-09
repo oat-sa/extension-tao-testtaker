3c1629b708ff18f8086c5f75cc91658c1923817c
bugalot <jerome@taotesting.com>
2018-08-09 13:11:08 +0200

    Bump version 3.10.2

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -83,6 +83,6 @@ class Updater extends \common_ext_ExtensionUpdater
 
             $this->setVersion('3.7.0');
         }
-        $this->skip('3.7.0', '3.10.1');
+        $this->skip('3.7.0', '3.10.2');
     }
 }
