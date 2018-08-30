6f458b205ffa98e809dbea3da7181c484238b37a
siwane <camille@taotesting.com>
2018-08-30 11:03:20 +0200

    Add tao helper callback to fetch lang by code

--- config/default/csvImporterCallbacks.php
+++ config/default/csvImporterCallbacks.php
@@ -6,7 +6,8 @@ return new oat\oatbox\config\ConfigurationService(array(
     'config' => array(
         'callbacks' => array(
             '*' => array('trim'),
-            GenerisRdf::PROPERTY_USER_PASSWORD => array('oat\taoTestTaker\models\CsvImporter::taoSubjectsPasswordEncode')
+            GenerisRdf::PROPERTY_USER_PASSWORD => array('oat\taoTestTaker\models\CsvImporter::taoSubjectsPasswordEncode'),
+            GenerisRdf::PROPERTY_USER_DEFLG => array('\tao_models_classes_LanguageService::filterLanguage'),
         ),
         'use_properties_for_event' => false
     )
