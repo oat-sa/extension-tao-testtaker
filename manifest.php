c6981faba10bfde63d0c18b76c16dcec94abe77f
Sergii <sergii@taotesting.com>
2019-03-19 13:20:29 +0100

    tao dependency updated

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return array(
     'version' => '5.2.1',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
-	    'tao' => '>=27.4.1',
+	    'tao' => '>=28.0.4',
 	    'taoBackOffice' => '>=3.0.0'
     ),
 	'models' => array(
