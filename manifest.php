bfd94b14fbfd63ee6b290606fbd48147abb99c09
alexey <aliakseiv@taotesting.com>
2019-03-27 17:56:37 +0300

    TAO-7830. $userId in tao_models_classes_import_ImportHandler::import implementation

--- manifest.php
+++ manifest.php
@@ -33,10 +33,10 @@ return array(
     'label' => 'Test-taker core extension',
 	'description' => 'TAO TestTaker extension',
     'license' => 'GPL-2.0',
-    'version' => '6.0.0',
+    'version' => '6.1.0',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
-	    'tao' => '>=30.0.2',
+	    'tao' => '>=31.0.0',
 	    'taoBackOffice' => '>=3.0.0'
     ),
 	'models' => array(
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -98,6 +98,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('3.11.0');
         }
 
-        $this->skip('3.11.0', '6.0.0');
+        $this->skip('3.11.0', '6.1.0');
     }
 }
