a7d366fe91e852e2728279fcce944b1577aa7526
Martin Nicholson <martin@taotesting.com>
2019-05-21 12:37:06 +0200

    Version bump

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'label' => 'Test-taker core extension',
 	'description' => 'TAO TestTaker extension',
     'license' => 'GPL-2.0',
-    'version' => '7.0.0',
+    'version' => '7.0.1',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
 	    'tao' => '>=34.0.0',
