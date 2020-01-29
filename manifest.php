42d4b3dcfd6ae6f379e3f85d3afec2b4074c8e58
Camille Moyon <camille@taotesting.com>
2020-01-29 11:24:52 +0100

    Bump version

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return [
     'label' => 'Test-taker core extension',
     'description' => 'TAO TestTaker extension',
     'license' => 'GPL-2.0',
-    'version' => '7.2.1',
+    'version' => '7.2.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=36.1.0',
